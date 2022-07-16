a963d483ea1bb25fb5469f5329677820bfffd5dd
Isaac Malin <isaac.malin@mykenzie.snhu.edu>
2022-07-16 14:03:12 -0600

    Second challenge complete

--- script.js
+++ script.js
@@ -7,5 +7,10 @@ let isNumberPostive = (x) => {
         console.log(false);
     }
 }
-
 isNumberPostive(-2);
+
+function convertDaysToAge(numOfDays) {
+    let age = numOfDays / 365;
+    console.log(age);
+}
+convertDaysToAge(7300);
