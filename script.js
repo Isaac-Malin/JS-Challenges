31be547d081c33744b17cdf3cfb8174c0f0f1ae1
Isaac Malin <isaac.malin@mykenzie.snhu.edu>
2022-07-16 14:07:38 -0600

    Third challenge complete

--- script.js
+++ script.js
@@ -14,3 +14,17 @@ function convertDaysToAge(numOfDays) {
     console.log(age);
 }
 convertDaysToAge(7300);
+
+function getLargetsNumber(num1, num2, num3) {
+    if (num1 > num2 && num1 > num3){
+        console.log(num1)
+    } else if (num2 > num1 && num2 > num3) {
+        console.log(num2)
+    } else {
+        console.log(num3)
+    }
+    }
+
+    getLargetsNumber(5,3,2);
+    getLargetsNumber(3,7,4);
+    getLargetsNumber(2,3,12);
